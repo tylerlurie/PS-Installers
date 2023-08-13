7dcdf1c9c4c49d9efd65c5b58f600efaf8c07de5
tylerlurie <141464744+tylerlurie@users.noreply.github.com>
2023-08-12 23:46:47 -0400

    Audacity

---
+++ Media/Install_Audacity.ps1
@@ -0,0 +1,8 @@
+$ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$dlLink = Invoke-WebRequest -UseBasicParsing -Uri "https://www.audacityteam.org/download/windows/" | Select-Object -ExpandProperty Links | Where-Object {$_.href -like "*audacity-win-*-x64.exe"} | Select-Object -ExpandProperty href | Select-Object -Last 1
+$file = $dlLink -split "/" | Select-Object -Last 1
+$InstallerPath = Join-Path $env:TEMP $file
+(New-Object System.Net.WebClient).DownloadFile($dlLink, $InstallerPath)
+Start-Process $InstallerPath -Wait -ArgumentList "/VERYSILENT /NORESTART" -Verb RunAs
+Remove-Item $InstallerPath
