6a744f08d3ff65c743e8315e17885501497d400b
tylerlurie <141464744+tylerlurie@users.noreply.github.com>
2025-03-10 22:16:26 -0400

    Opera now correctly installs the latest version

--- Web Browsers/Install_Opera.ps1
+++ Web Browsers/Install_Opera.ps1
@@ -1,8 +1,9 @@
 $ProgressPreference = 'SilentlyContinue'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$version = (Invoke-WebRequest -UseBasicParsing -Uri "https://get.geo.opera.com/pub/opera/desktop" | Select-Object -ExpandProperty Links | Select-Object -ExpandProperty href | Select-Object -Last 1) -replace "/", ""
-$file = "Opera_$($version)_Setup_x64.exe"
-$dlLink = "https://get.geo.opera.com/pub/opera/desktop/$version/win/$file"
+$versions = (Invoke-WebRequest -UseBasicParsing -Uri "https://get.geo.opera.com/pub/opera/desktop" | Select-Object -ExpandProperty Links | Select-Object -ExpandProperty href)
+$latestVersion = $versions | Where-Object { $_ -match '\d+\.\d+\.\d+\.\d+' } | ForEach-Object { if ($_ -match '(\d+\.\d+\.\d+\.\d+)') { $matches[1] } } | Sort-Object { [version]$_ } | Select-Object -Last 1
+$file = "Opera_$($latestVersion)_Setup_x64.exe"
+$dlLink = "https://get.geo.opera.com/pub/opera/desktop/$latestVersion/win/$file"
 $InstallerPath = Join-Path $env:TEMP $file
 (New-Object System.Net.WebClient).DownloadFile($dlLink, $InstallerPath)
 Start-Process $InstallerPath -Wait -ArgumentList "/silent /allusers=1 /launchopera=0 /enable-installer-stats=0 /enable-stats=0" -Verb RunAs
