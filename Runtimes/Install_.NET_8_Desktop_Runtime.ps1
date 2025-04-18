82991bd9e08ee34b09bc0924f7d62252d236da84
tylerlurie <141464744+tylerlurie@users.noreply.github.com>
2025-04-17 23:51:29 -0400

    Fix a typo
    Thanks Mattias

--- Runtimes/Install_.NET_8_Desktop_Runtime.ps1
+++ Runtimes/Install_.NET_8_Desktop_Runtime.ps1
@@ -1,9 +1,12 @@
 $ProgressPreference = 'SilentlyContinue'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$baseLink = "https://dotnet.microsoft.com" + (Invoke-WebRequest -Uri "https://dotnet.microsoft.com/en-us/download/dotnet/8.0" -UseBasicParsing | Select-Object -ExpandProperty Links | Where-Object {($_.href -like "*runtime-desktop-7.[0-9]*-windows-x64-installer") -and ($_.href -notlike "*preview*")} | Select-Object -ExpandProperty href)[0]
-$dlLink = ((Invoke-WebRequest -uri $baseLink -UseBasicParsing | Select-Object -ExpandProperty Links | Where-Object {($_.href -like "*.exe")}).href)[0]
+$response = Invoke-WebRequest -Uri "https://dotnet.microsoft.com/en-us/download/dotnet/8.0" -UseBasicParsing
+$baseLink = "https://dotnet.microsoft.com" + ($response.Links | Where-Object {($_.href -like "*runtime-desktop-8.[0-9]*-windows-x64-installer") -and ($_.href -notlike "*preview*")} | Select-Object -ExpandProperty href)[0]
+$dlLinkResponse = Invoke-WebRequest -Uri $baseLink -UseBasicParsing
+$dlLink = ($dlLinkResponse.Links | Where-Object {($_.href -like "*.exe")}).href[0]
 $file = ($dlLink -split "/")[-1]
 $InstallerPath = Join-Path $env:TEMP $file
+if (-not (Test-Path -Path $env:TEMP)) { New-Item -ItemType Directory -Path $env:TEMP }
 (New-Object System.Net.WebClient).DownloadFile($dlLink, $InstallerPath)
 Start-Process "$InstallerPath" -Wait -ArgumentList "/install /quiet /norestart" -Verb RunAs
 Remove-Item $InstallerPath
